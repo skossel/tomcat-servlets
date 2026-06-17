e67b95d7cec83bd1122e86295c3c77278a38d1c0
Sandro Kossel <sandro.kossel@ergon.ch>
2026-06-17 12:09:03 +0200

    replace single-page-class routing with dynamic package scanning to support multiple page classes

--- src/main/java/ch/ergon/sandro/tomcattest/MyWebApp.java
+++ src/main/java/ch/ergon/sandro/tomcattest/MyWebApp.java
@@ -1,12 +1,15 @@
 package ch.ergon.sandro.tomcattest;
 
-import ch.ergon.sandro.tomcattest.pages.PageClass;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @WebServlet(name = "myWebApp", urlPatterns = {"/*"})
@@ -32,22 +35,56 @@ public class MyWebApp extends HttpServlet {
     private boolean handleRouting(String path, HttpServletResponse response) throws Exception {
         if (path == null || path.length() < 2) return false;
 
-        var pageClass = PageClass.class;
-        for (var method : pageClass.getDeclaredMethods()) {
-            var pathAttr = method.getAnnotation(Path.class);
-            if (pathAttr != null && pathAttr.value().equals(path)) {
-                var dsoFileName = (String) method.invoke(pageClass.getDeclaredConstructor().newInstance());
-                var html = loadFile("/ch/ergon/sandro/tomcattest/pages/" + dsoFileName);
-                if (html == null) {
-                    response.sendError(404, "file not found: " + dsoFileName);
+        for (Class<?> clazz : getClasses("ch.ergon.sandro.tomcattest.pages")) {
+            for (var method : clazz.getDeclaredMethods()) {
+                var pathAttr = method.getAnnotation(Path.class);
+                if (pathAttr != null && pathAttr.value().equals(path)) {
+                    var dsoFileName = (String) method.invoke(clazz.getDeclaredConstructor().newInstance());
+                    var html = loadFile("/ch/ergon/sandro/tomcattest/pages/" + dsoFileName);
+                    if (html == null) {
+                        response.sendError(404, "file not found: " + dsoFileName);
+                        return true;
+                    }
+                    response.setContentType("text/html;charset=UTF-8");
+                    response.getWriter().write(html);
                     return true;
                 }
-                response.setContentType("text/html;charset=UTF-8");
-                response.getWriter().write(html);
-                return true;
             }
         }
         return false;
+    }
+
+    private static Class[] getClasses(String packageName) throws ClassNotFoundException, IOException {
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        String path = packageName.replace('.', '/');
+        Enumeration<URL> resources = classLoader.getResources(path);
+        List<File> dirs = new ArrayList<>();
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            dirs.add(new File(resource.getFile()));
+        }
+        ArrayList<Class> classes = new ArrayList<>();
+        for (File directory : dirs) {
+            classes.addAll(findClasses(directory, packageName));
+        }
+        return classes.toArray(new Class[0]);
+    }
+
+    private static List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
+        List<Class> classes = new ArrayList<>();
+        if (!directory.exists()) {
+            return classes;
+        }
+        File[] files = directory.listFiles();
+        if (files == null) return classes;
+        for (File file : files) {
+            if (file.isDirectory()) {
+                classes.addAll(findClasses(file, packageName + "." + file.getName()));
+            } else if (file.getName().endsWith(".class")) {
+                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
+            }
+        }
+        return classes;
     }
 
     private String loadFile(String path) throws IOException {
