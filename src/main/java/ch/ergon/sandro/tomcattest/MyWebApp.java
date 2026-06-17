76ad654ab5112d017d396055178222223a4a7576
Sandro Kossel <sandro.kossel@ergon.ch>
2026-06-17 16:17:48 +0200

    refactor `getClasses` method: rename variables for clarity, add null checks, and improve directory handling with assertions

--- src/main/java/ch/ergon/sandro/tomcattest/MyWebApp.java
+++ src/main/java/ch/ergon/sandro/tomcattest/MyWebApp.java
@@ -56,16 +56,17 @@ public class MyWebApp extends HttpServlet {
 
     private static Class[] getClasses(String packageName) throws ClassNotFoundException, IOException {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        assert classLoader != null;
         String path = packageName.replace('.', '/');
         Enumeration<URL> resources = classLoader.getResources(path);
-        List<File> dirs = new ArrayList<>();
+        List<File> files = new ArrayList<>();
         while (resources.hasMoreElements()) {
             URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
+            files.add(new File(resource.getFile()));
         }
         ArrayList<Class> classes = new ArrayList<>();
-        for (File directory : dirs) {
-            classes.addAll(findClasses(directory, packageName));
+        for (File file : files) {
+            classes.addAll(findClasses(file, packageName));
         }
         return classes.toArray(new Class[0]);
     }
@@ -79,6 +80,7 @@ public class MyWebApp extends HttpServlet {
         if (files == null) return classes;
         for (File file : files) {
             if (file.isDirectory()) {
+                assert !file.getName().contains(".");
                 classes.addAll(findClasses(file, packageName + "." + file.getName()));
             } else if (file.getName().endsWith(".class")) {
                 classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
